182310a341b6c9bccf123e262e9bb795ca554582
KFeyn <kir-fein@yandex.ru>
2020-11-05 03:07:30 +0300

    Исправлена ошибка с дублированием заголовка после каждого отзыва

--- sel.py
+++ sel.py
@@ -7,8 +7,8 @@ def review_downloader(product_link):
     # Логинимся в яндексе
     browser = webdriver.Chrome('/Users/feyn/Documents/Мои дурацкие творения/reviews negative/chromedriver')
 
-    login = 'kir-fein'
-    password = '6_@LVssu@4T#J+T'
+    login = ''
+    password = ''
 
     login_link = 'https://passport.yandex.ru/auth'
 
@@ -25,15 +25,23 @@ def review_downloader(product_link):
 
     time.sleep(1)
 
-    # Бегаем по страничкам с отзывами и записываем негативные в файл
+    # Достаем название продукта
+
     all_troubles = ''
+
+    browser.get(product_link + 'reviews')
+
+    product_name = browser.find_elements_by_tag_name('h1')[0].text
+    all_troubles += product_name
+
+    time.sleep(1)
+
+    # Бегаем по страничкам с отзывами и записываем негативные в файл
 
     for i in range(1, 1000):
 
         browser.get(product_link + 'reviews?page=' + str(i))
 
-        product_name = browser.find_elements_by_tag_name('h1')[0].text
-        all_troubles += product_name
         all_troubles += '\n\n'
 
         trouble = browser.find_elements_by_xpath("//dt[text()='Недостатки: ']/following-sibling::dd")
